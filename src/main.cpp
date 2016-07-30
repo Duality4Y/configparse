824ba3da4616650ff21507ea99d7744aebed8280
Duality4y <201292@live.nl>
2016-07-30 02:05:24 +0200

    building a lexer i think or atleast what i think is a lexer

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,136 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
-#include <typeinfo>
+#include <locale>
 
-#define UNUSED(X) ((void)X)
+#pragma GCC diagnostic warning "-Wunused"
+#define STR_IT(X) #X 
 
-using std::cout;
-using std::endl;
+using namespace std;
 
-std::string config = "configs/test.conf";
+std::string configfile = "configs/test.conf";
 
-class Node
-{
-public:
-    Node(std::string *, std::string *);
-    template <typename T> T as();
-private:
-    std::string *name;
-    std::string *value;
-    std::vector<Node> subnodes;
-    std::vector<Node> nextnodes;
+// symbol definitions.
+enum TOKENS {
+    COLON,
+    L_BRACE,
+    R_BRACE,
+    POUND,
+    COMMA,
+    INDENT,
+    OBJ,
+    ARG,
+    VAL
 };
 
-Node::Node(std::string *name, std::string *value)
+vector<std::string> tokenstr =
 {
-    UNUSED(name);
-    UNUSED(value);
-}
-
-template <typename T>
-T Node::as()
-{
-    cout << "unsupported type: " << typeid(T).name() << " <Exiting>" << endl;
-    exit(1);
-}
-
-template <>
-int Node::as<int>()
-{
-    cout << "a value of int was requested." << endl;
-    return 0x64;
-}
-
-
-class Parser
-{
-public:
-    Parser(std::string);
-    void process();
-    std::vector<Node> top_nodes;
-private:
-    std::string configfile;
+    STR_IT(COLON),
+    STR_IT(L_BRACE),
+    STR_IT(R_BRACE),
+    STR_IT(POUND),
+    STR_IT(COMMA),
+    STR_IT(INDENT),
+    STR_IT(OBJ),
+    STR_IT(ARG),
+    STR_IT(VAL)
 };
 
-Parser::Parser(std::string config)
+/*
+    the Lexer creates a array of tokens.
+    each might have a value, and they contain
+    which token they are (like COLON for ':')
+*/
+typedef struct
 {
-    this->configfile = config;
-    // test some nodes.
-    std::string aname = "Hello";
-    std::string avalue = "World";
-    Node a = Node(&aname, &avalue);
-    a.as<int>();
-    std::string atest = a.as<std::string>();
+    std::string value;
+    TOKENS token;
+} token_t;
+
+class Lexer
+{
+public:
+    Lexer(std::string);
+    char get();
+    char peek();
+    vector<token_t> lex();
+    token_t get_token();
+
+private:
+
+    vector<token_t> tokens;
+    std::ifstream infile;
+};
+
+Lexer::Lexer(std::string file)
+{
+    this->infile = std::ifstream(file);
 }
 
-void Parser::process()
+vector<token_t> Lexer::lex()
 {
+    // token_t test_token = {"TEST", COLON};
+    // this->tokens.push_back(test_token);
+    token_t current_token;
 
+    while(this->infile.good())
+    {
+        // start of object ?
+        if(this->peek() == '#')
+        {
+            std::cout << "WELL A COMMENT\n";
+            while(this->peek() != '\n')
+            {
+                this->get();
+            }
+        }
+        else if(std::isalpha(this->peek()) && !std::isspace(this->peek()))
+        {
+            current_token.value = "";
+            while(this->peek() != ':')
+            {
+                if(!std::isspace(this->peek()))
+                {
+                    current_token.value += this->get();
+                }
+                else
+                {
+                    this->get();
+                }
+            }
+            current_token.token = OBJ;
+            this->tokens.push_back(current_token);
+        }
+        this->get();
+    }
+
+    return this->tokens;
+}
+
+char Lexer::get()
+{
+    return (char)this->infile.get();
+}
+
+char Lexer::peek()
+{
+    return (char)this->infile.peek();
 }
 
 int main(void)
 {
-    Parser confparse = Parser(config);
+    Lexer lexer = Lexer(configfile);
+    vector<token_t> tokens = lexer.lex();
+
+    // for(size_t i = 0; i < tokens.size(); i++)
+    // {
+    //     std::cout << tokens.value << ":" << tokens.token << std::endl;
+    // }
+
+    for(auto token: tokens)
+    {
+        std::cout << token.value << ":" << tokenstr[token.token] << std::endl;
+    }
+
     return 0;
 }
